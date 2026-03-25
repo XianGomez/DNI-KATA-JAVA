31deb6456faa3777edaf8261bb0c28c08095ef62
XianGomez <xiiangomezz@gmail.com>
2026-03-25 10:33:47 +0100

    feat(TablaAsignacion): Crear constructor, funcion getLetra y isLetraPermitida

---
+++ src/main/java/edu/teamrocket/TablaAsignacion.java
@@ -0,0 +1,21 @@
+package edu.teamrocket;
+
+public class TablaAsignacion {
+    
+    public char[] tabla = {'T', 'R', 'W', 'A', 'G', 'M', 
+					 		'Y', 'F', 'P', 'D', 'X', 'B', 
+							'N', 'J', 'Z', 'S', 'Q', 'V', 
+							'H', 'L', 'C', 'K', 'E'};
+
+    public TablaAsignacion() {};
+
+    public char getLetra(int posicion) {        
+        return this.tabla[posicion];
+    }
+
+    public boolean isLetraPermitida(char letra) {
+        return new String(this.tabla).indexOf(letra) != -1;
+    }
+    
+    
+}
